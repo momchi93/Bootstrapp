136681f1764ddf7790382e644a7aad5e80502926
momchi93 <momchi93@gmail.com>
2018-11-26 12:47:37 +0100

    Update AR_Model1.py

--- AR_Model1.py
+++ AR_Model1.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 
 def periodogramm(inputt, samples):
     t = np.arange(samples)
-    N = int(np.floor(samples/2))
-    omega = 2 * np.pi * np.arange(N+1) / samples
+    N = int(np.floor(samples / 2))
+    omega = 2 * np.pi * np.arange(N + 1) / samples
     Pxx_Density = np.zeros(N + 1)
     for i1 in range(N + 1):
-        Pxx_Density[i1] = (1/samples) * np.abs(np.sum(inputt * np.exp(-1j * omega[i1] * t)))**2
+        Pxx_Density[i1] = (1 / samples) * np.abs(np.sum(inputt * np.exp(-1j * omega[i1] * t))) ** 2
     return Pxx_Density
 
 
@@ -21,13 +21,13 @@ def kernel_function(arg):
 
 
 def spectral_density_estimator(h1, powerspec, samples):
-    N = powerspec.size-1
-    omega = 2 * np.pi * np.arange(N+1) / samples
+    N = powerspec.size - 1
+    omega = 2 * np.pi * np.arange(N + 1) / samples
     SpectralDensityEstimate = np.zeros(N + 1)
     for i1 in range(N + 1):
         kernel = np.zeros(N + 1)
         for i2 in range(N + 1):
-                kernel[i2] = kernel_function((omega[i1] - omega[i2]) / h1)
+            kernel[i2] = kernel_function((omega[i1] - omega[i2]) / h1)
         SpectralDensityEstimate[i1] = sum((kernel * powerspec) / (h1 * samples))
     return SpectralDensityEstimate
 
@@ -50,7 +50,7 @@ def bootstrap_estimate(res, g, sde_centered, samples):
 
 np.random.seed(123)
 n_samples = 256
-n = int((n_samples/2)+1)
+n = int((n_samples / 2) + 1)
 a1 = 0.5
 a2 = -0.6
 a3 = 0.3
@@ -59,7 +59,7 @@ a5 = 0.2
 alpha = 0.05
 h = g = 0.05
 x = noise = np.random.normal(size=n_samples)
-w = (2 * np.pi * np.arange(n))/n_samples
+w = (2 * np.pi * np.arange(n)) / n_samples
 for t in range(5, n_samples):
     x[t] = a1 * x[t - 1] + a2 * x[t - 2] + a3 * x[t - 3] + a4 * x[t - 4] + a5 * x[t - 5] + noise[t]
 
@@ -74,8 +74,8 @@ Cxx_estimate_centered = spectral_density_estimator(h, Ixx_density_centered, n_sa
 
 # Step 3 Compute and Rescale Residuals
 
-residuals = Ixx_density_centered/Cxx_estimate_centered
-residuals_rescaled = residuals/np.mean(residuals)
+residuals = Ixx_density_centered / Cxx_estimate_centered
+residuals_rescaled = residuals / np.mean(residuals)
 
 # Step 4,5 & 6 Bootstrap Residuals and Estimate
 b_samples = 100
@@ -85,16 +85,16 @@ for c in range(b_samples):
 
 # Step 7 Confidence Interval Estimation
 
-upper_index = int(np.ceil((b_samples-1)*(1-alpha)))
-lower_index = int(np.floor((b_samples-1)*alpha))
+upper_index = int(np.ceil((b_samples - 1) * (1 - alpha)))
+lower_index = int(np.floor((b_samples - 1) * alpha))
 
-print(b_sde)
 b_sde.sort(axis=0)
-print(b_sde)
+
 Cxx_estimate_centered_upper = b_sde[upper_index, :]
 Cxx_estimate_centered_lower = b_sde[lower_index, :]
 
-plt.plot(w, Cxx_estimate_centered)
-plt.plot(w, Cxx_estimate_centered_upper)
-plt.plot(w, Cxx_estimate_centered_lower)
+plt.plot(w, Cxx_estimate_centered, label='Spectral Density Estimate')
+plt.plot(w, Cxx_estimate_centered_upper, label='upper bound')
+plt.plot(w, Cxx_estimate_centered_lower, label='lower bound')
+plt.legend(loc='best')
 plt.show()
