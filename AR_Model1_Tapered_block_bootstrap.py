2a145b4e64c5b8b528281b244320c34ef99c50c1
MomchilMac <momchi93@gmail.com>
2018-11-29 03:48:08 +0100

    Update AR_Model1_Tapered_block_bootstrap.py
    added step 5, working on 6

--- AR_Model1_Tapered_block_bootstrap.py
+++ AR_Model1_Tapered_block_bootstrap.py
@@ -14,8 +14,8 @@ def periodogramm(inputt, samples):
     N = int(np.floor(samples / 2))
     omega = 2 * np.pi * np.arange(N + 1) / samples
     Pxx_Density = np.zeros(N + 1)
-    for i1 in range(N + 1):
-        Pxx_Density[i1] = (1 / samples) * np.abs(np.sum(inputt * np.exp(-1j * omega[i1] * t))) ** 2
+    for i3 in range(N + 1):
+        Pxx_Density[i3] = (1 / samples) * np.abs(np.sum(inputt * np.exp(-1j * omega[i3] * t))) ** 2
     return Pxx_Density
 
 
@@ -39,10 +39,36 @@ def spectral_density_estimator(h1, powerspec, samples):
     return SpectralDensityEstimate
 
 
+def draw_block():
+    x_block = np.zeros(b)
+    i = np.random.choice(set_for_i)
+    for j in range(b):
+        x_block[j] = tp_w[j] * const * x_centered[i + j]
+    return x_block
+
+
+def get_resample():
+    X_resamples_list = []
+    for p in range(k):
+        block = draw_block()
+        X_resamples_list.append(block)
+    X_resamples = np.concatenate(np.array(X_resamples_list), axis=None)
+    return X_resamples
+
+
 n_samples = 256
 n = int(np.floor(n_samples / 2))
 h = g = 0.05
+b = 38
+b_samples = 30
 ar = np.array([0.5, -0.6, 0.3, -0.4, 0.2])
+set_for_i = np.arange(1, n_samples - b + 2)  # integers representing start of a block
+w1 = np.arange(1, 17) / 16
+w2 = np.ones(6)
+w3 = np.flip(w1, axis=None)
+tp_w = np.concatenate((w1, w2, w3), axis=None)  # tapering window
+tp_w_2 = np.sqrt(np.sum(tp_w))  # Sum of tapering windows, squared
+const = np.sqrt(b) / tp_w_2
 
 # Generate AR(5) Series
 x = generate_ar_process(n_samples, ar)
@@ -55,14 +81,26 @@ Ixx_density_centered = periodogramm(x_centered, n_samples)
 Cxx_estimate_centered = spectral_density_estimator(h, Ixx_density_centered, n_samples)
 
 # Step 3 Set the bootstrap parameters
-b = 38
 k = int(np.floor(n_samples / b))
+l = k * b
+l_half = int(l / 2)
 
+# Step 6 Repeat Step 4 & 5
+Cxx_estimate_centered_b = np.zeros([b_samples, l_half + 1])
 
-# Step 4 Draw Bootstrap Resamples
-set_for_i = np.arange(1, n_samples-b+2)
-i = np.zeros(k)
-for p in range(k):
-    i[p] = np.random.choice(set_for_i)
-m = np.arange(k)
+for b in range(b_samples):
+    # Step 4 Draw Bootstrap Resamples
+    X_resample = get_resample()
+    # Step 5 Construct Bootstrap Estimate
+    X_resamples_centered = X_resample - np.mean(X_resample)
+    #Ixx_density_centered_b = periodogramm(X_resamples_centered, l)
+    #Ixx_density_centered_b[0] = 0
+    #Cxx_estimate_centered_b[b, :] = spectral_density_estimator(h, Ixx_density_centered_b, l)
 
+w = 2 * np.pi * np.arange(n + 1) / n_samples
+w1 = 2 * np.pi * np.arange(l_half + 1) / l
+#plt.plot(w, Cxx_estimate_centered)
+#plt.plot(w1, Cxx_estimate_centered_b)
+#plt.show()
+
+print('finish')
